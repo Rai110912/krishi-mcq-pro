6bbf9edda45199d3044b6829231a831060988a9b
Rai110912 <Rai110912@users.noreply.github.com>
2026-05-24 20:17:39 +0545

    Complete full lightweight QA and debugging pass on Firebase-hosted PWA, increment to v19

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'krishi-mcq-pro-v18';
+const CACHE_NAME = 'krishi-mcq-pro-v19';
 
 // Install Event: Pre-cache core shell resources with cache-busting reload
 self.addEventListener('install', event => {
